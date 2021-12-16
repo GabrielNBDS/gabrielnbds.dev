2417c8e3a2c9887b3511d99442cb49e4f1f089f4
GabrielNBDS <gabriel.nbds@gmail.com>
2021-12-15 22:54:47 -0300

    👌 IMPROVE: Added padding to match with header

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,20 +3,20 @@ import Link from 'next/link'
 
 export default function Custom404() {
   return (
-    <Container>
+    <Container py={32}>
       <Heading mb={4} fontSize="20px">
         Page not found (404)
       </Heading>
 
       <Text>
         The page you&apos;re looking for may have moved or no longer exists.
-        Head back to our{' '}
+        Head back to{' '}
         <Link href="/">
           <chakra.a cursor="pointer" fontWeight="600">
             homepage
           </chakra.a>
         </Link>
-        , or take a look at one of the sections below.
+        .
       </Text>
     </Container>
   )
