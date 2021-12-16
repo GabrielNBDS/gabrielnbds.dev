8ea6961fdf8772b00722160a77393891a221eb09
GabrielNBDS <gabriel.nbds@gmail.com>
2021-12-15 23:39:17 -0300

    📦 NEW: Created work component

---
+++ src/components/home/work.tsx
@@ -0,0 +1,20 @@
+import { Heading } from '@chakra-ui/react'
+import Paragraph from '../paragraph'
+
+const Work = () => {
+  return (
+    <>
+      <Heading as="h3" variant="section-title">
+        Work
+      </Heading>
+      <Paragraph>
+        Gabriel is a full-stack developer at IBM based in Brazil who loves to
+        create new solutions. He&apos;s proficient in the React/Node Stack but
+        has no fear of learning new techs ( except fortran 😬 ). Apart from
+        coding he also likes music and motorsport.
+      </Paragraph>
+    </>
+  )
+}
+
+export default Work
